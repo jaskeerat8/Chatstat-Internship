1ac58d39cde1f07fdf001fe3b85e3a43a9dfbdbb
jaskeerat8 <32131898+jaskeerat8@users.noreply.github.com>
2023-09-28 03:12:12 +1000

    Update raw_to_intermediate.py

--- raw_to_intermediate.py
+++ raw_to_intermediate.py
@@ -20,7 +20,7 @@ def file_data(s3_raw_loc, yesterday_date, current_date, collection):
     df_list = []
     for prefix in prefixes:
         for obj in bucket.objects.filter(Prefix = prefix):
-            body = obj.get()['Body'].read()
+            body = obj.get()["Body"].read()
             df = pd.read_csv(io.BytesIO(body), encoding = 'utf8')        
             df_list.append(df)
     
@@ -38,8 +38,8 @@ secret_name = "dashboard"
 current_date = datetime.now().strftime("%d-%m-%Y")
 yesterday_date = (datetime.now() - timedelta(days=1)).strftime("%d-%m-%Y")
 
-session = boto3.session.Session(aws_access_key_id = "AKIAQBTIQ6VDCHHWNCNV", aws_secret_access_key = "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")
-sm_client = session.client(service_name = "secretsmanager", region_name = region_name)
+session = boto3.session.Session(region_name = region_name)
+sm_client = session.client(service_name = "secretsmanager")
 s3_client = session.resource("s3")
 
 
@@ -55,6 +55,7 @@ s3_inter_loc = value["inter_location"]
 
 
 #Reading data from RAW
+users_df = file_data(s3_raw_loc, yesterday_date, current_date, "users")
 childrens_df = file_data(s3_raw_loc, yesterday_date, current_date, "childrens")
 accounts_df = file_data(s3_raw_loc, yesterday_date, current_date, "accounts")
 contents_df = file_data(s3_raw_loc, yesterday_date, current_date, "contents")
@@ -62,14 +63,15 @@ comments_df = file_data(s3_raw_loc, yesterday_date, current_date, "comments")
 
 
 #Merging Data
-childrens_accounts_df = pd.merge(childrens_df, accounts_df, left_on = "accounts_childrens", right_on = "_id_accounts", how = "inner")
-childrens_accounts_contents_df =  pd.merge(childrens_accounts_df, contents_df, left_on = "content_accounts", right_on = "_id_contents", how = "inner")
-childrens_accounts_contents_comments_df = pd.merge(childrens_accounts_contents_df, comments_df, left_on = "comments_contents", right_on = "_id_comments", how = "left")
+users_childrens_df = pd.merge(users_df, childrens_df, left_on = "children_users", right_on = "_id_childrens", how = "inner")
+users_childrens_accounts_df = pd.merge(users_childrens_df, accounts_df, left_on = "accounts_childrens", right_on = "_id_accounts", how = "inner")
+users_childrens_accounts_contents_df =  pd.merge(users_childrens_accounts_df, contents_df, left_on = "content_accounts", right_on = "_id_contents", how = "inner")
+users_childrens_accounts_contents_comments_df = pd.merge(users_childrens_accounts_contents_df, comments_df, left_on = "comments_contents", right_on = "_id_comments", how = "left")
 
-childrens_accounts_contents_comments_df.drop_duplicates(inplace = True)
-new_columns = {old_col: old_col.lstrip("_") for old_col in childrens_accounts_contents_comments_df.columns}
-childrens_accounts_contents_comments_df = childrens_accounts_contents_comments_df.rename(columns = new_columns)
-childrens_accounts_contents_comments_df.fillna(value = "", inplace = True)
+users_childrens_accounts_contents_comments_df.drop_duplicates(inplace = True)
+new_columns = {old_col: old_col.lstrip("_") for old_col in users_childrens_accounts_contents_comments_df.columns}
+users_childrens_accounts_contents_comments_df = users_childrens_accounts_contents_comments_df.rename(columns = new_columns)
+users_childrens_accounts_contents_comments_df.fillna(value = "", inplace = True)
 
 
 #Saving to s3 location
@@ -77,6 +79,6 @@ s3_client = session.client("s3")
 bucket = s3_inter_loc.split("/")[2]
 intermediate_data_key = "/".join(s3_inter_loc.split("/")[3:]) + f"{current_date}/intermediate_{datetime.now()}.csv"
 
-csv_string = childrens_accounts_contents_comments_df.to_csv(index = False)
+csv_string = users_childrens_accounts_contents_comments_df.to_csv(index = False)
 s3_client.put_object(Bucket = bucket, Key = intermediate_data_key, Body = csv_string)
 print(f"Saving file to:", "Bucket:", bucket, "Key:", intermediate_data_key)
