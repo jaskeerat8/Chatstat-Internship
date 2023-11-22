6bb0fbbf35e10af404833e00c3606402dee69ecf
jaskeerat8 <32131898+jaskeerat8@users.noreply.github.com>
2023-11-22 11:48:44 +1000

    Update comment_collection.py

--- comment_collection.py
+++ comment_collection.py
@@ -4,9 +4,9 @@ import json
 import boto3
 import pymongo
 import pandas as pd
-from sshtunnel import SSHTunnelForwarder
 from datetime import datetime
 from bson import ObjectId
+
 
 #Setting up the Final Result
 def final_result(x):
@@ -19,6 +19,7 @@ def final_result(x):
     else:
         return str(x).title().strip()
 
+    
 #Setting up Boto3 Client
 region_name = "ap-southeast-2"
 secret_name = "dashboard"
@@ -29,6 +30,7 @@ current_date = datetime.now().strftime("%d-%m-%Y")
 session = boto3.session.Session(region_name = region_name)
 sm_client = session.client(service_name = "secretsmanager")
 s3_client = session.resource("s3")
+ec2_client = session.client(service_name = "ec2")
 
 
 #Reading Data from Secrets Manager
@@ -38,21 +40,14 @@ try:
 except Exception as e:
     print(e)
 
-remote_server_ip = value["remote_server_ip"]
-remote_server_username = value["remote_server_username"]
-remote_server_password = value["remote_server_password"]
 
-server = SSHTunnelForwarder(
-    ssh_address_or_host = (remote_server_ip, 22),
-    ssh_username = remote_server_username,
-    ssh_password = remote_server_password,
-    remote_bind_address = ('localhost', 9017),
-    local_bind_address = ('localhost', 9017),
-    ssh_pkey = None
-)
-server.start()
+#Getting Public IP of EC2 server
+ec2_response = ec2_client.describe_instances(InstanceIds=[value["ec2_instance_id"]])
+ec2_ip = ec2_response["Reservations"][0]["Instances"][0].get("PublicIpAddress")
 
-mongo_client = pymongo.MongoClient(f"""mongodb://{value["mongodb_user"]}:{value["mongodb_password"]}@localhost:9017/{value["mongodb_database"]}""")
+
+#Connecting to Mongodb
+mongo_client = pymongo.MongoClient(f"""mongodb://{value["mongodb_user"]}:{value["mongodb_password"]}@{ec2_ip}:{value["mongodb_port"]}/{value["mongodb_database"]}""")
 db = mongo_client[value["mongodb_database"]]
 
 collection_names = db.list_collection_names()
@@ -93,7 +88,6 @@ s3_client.put_object(Bucket = bucket, Key = key, Body = csv_string)
 print(f"Completed {collection} Collection, Saving file to:", "Bucket:", bucket, "Key:", key)
 
 
-# Stop the tunnel
+#Close the Clients
 comments_cursor.close()
 mongo_client.close()
-server.stop()
